0609f23fd19d55f02875bd0cada3733fab942770
rasulovvv <rasulovasadbek1983@gmail.com>
2022-02-22 02:08:08 +0500

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,10 @@
+var dollars = 10900.00;
+var euro = 12272.64;
+var equal = (dollars * 750 + euro * 120)
+console.log(equal)
+var myMoney = prompt("Qancha pulingiz bor? Kiriting: ")
+if(myMoney >= equal){
+    alert("Oq yo'l, Birodar")
+}else{
+    alert("Ey boirodar, ozgina sabr qilish kerak bo'lar ekan")
+}
